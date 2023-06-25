e00dce30465afdc53b0a6bff1f9ce6ea5bdfd644
Scott Fanetti <scott.fanetti@gmail.com>
2023-06-25 17:17:39 -0400

    added an arbitrary chnge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a change - there are many like it but this one is mine
+          
         </p>
         <a
           className="App-link"
